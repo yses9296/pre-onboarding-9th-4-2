b9a439f34b1d8e453aaaccf58f019dc509365065
Eunseo Choi <yses9296@gmail.com>
2023-04-17 20:14:36 +0900

    refactor: 테스트 코드 데이터 패칭 일치 여부

--- setupTest.js
+++ setupTest.js
@@ -2,9 +2,11 @@ import { expect, afterEach } from "vitest";
 import { cleanup, configure } from "@testing-library/react";
 import matchers from "@testing-library/jest-dom/matchers";
 import "@testing-library/jest-dom";
+import { fetch } from "cross-fetch";
 
 configure({ asyncUtilTimeout: 5000 });
-global.fetch = vi.fn();
+// global.fetch = vi.fn();
+global.fetch = fetch;
 
 expect.extend(matchers);
 afterEach(() => {
--- src/__test__/OrderTableBoard.test.tsx
+++ src/__test__/OrderTableBoard.test.tsx
@@ -1,8 +1,28 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { ProvidersWrapper } from "./test.wrapper";
 import AdminPage from "../pages/AdminPage";
+import { fetchOrderData } from "../apis/fetchOrderData";
+import mockData from "./data/mockData.json";
+
+const VITE_DATA_URL =
+  "https://file.notion.so/f/s/8c82d2f4-c58e-4329-a41e-972dfcc7e976/mock_data.json?id=a7efc5d9-b10c-4be6-a9b3-5b872438c547&table=block&spaceId=72b256b1-ae08-4e70-bb6c-f9c3cad5a793&expirationTimestamp=1680789756626&signature=8AWjVrxkPwe5OS1AlYq6Vy5Tvs6vHZfAzgpuAjLJ6fc&downloadName=mock_data.json";
 
 describe("OrderTableBoard", () => {
+  //FETCH ORDER
+  it("should has fatched order data", async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve(mockData),
+    });
+
+    const data = await fetchOrderData();
+
+    expect(data).toEqual(mockData);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(VITE_DATA_URL);
+  });
+
   //STATUS TEST
   it("should filter status true when true option selected", async () => {
     render(
@@ -15,6 +35,7 @@ describe("OrderTableBoard", () => {
     const option = screen.getAllByRole("option");
 
     const tableBody = screen.getByLabelText("table-body");
+    const statusTrue = await screen.findAllByLabelText("status-true");
     const statusFalse = await screen.findAllByLabelText("status-false");
     const optionTrue = screen.getByRole("option", { name: "status-true" });
 
@@ -25,6 +46,7 @@ describe("OrderTableBoard", () => {
     expect(option).toHaveLength(3);
 
     await waitFor(() => {
+      // expect(tableBody).toContain(statusTrue);
       expect(tableBody).not.toContain(statusFalse);
     });
   }, 20000);
